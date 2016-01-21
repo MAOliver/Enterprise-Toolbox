37073b53a5096b6d0ef60bba999db0c2152584d8
Oliver <matthew.oliver@fnf.com>
2016-01-20 22:45:12 -0500

    removed unnecessary lib

--- source/auth/shared/Auth.Owin.ResourceAuthorization/IResourceAuthorizationManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Auth.Owin.ResourceAuthorization
-{
-    public interface IResourceAuthorizationManager
-    {
-        Task<bool> CheckAccessAsync(ResourceAuthorizationContext context);
-    }
-}
